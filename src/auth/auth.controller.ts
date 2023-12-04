5ae25e27780953a1ca612cb3efebe88dae940a72
aryan <aryan.jand@gmail.com>
2023-12-03 21:41:21 -0800

    added console log

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,8 +8,6 @@ import {
   Session,
   UseFilters,
 } from "@nestjs/common";
-
-import { Response } from "express";
 import { UserSession } from "../common";
 import { AuthService } from "./auth.service";
 import { SignInDto, SignUpDto } from "./dto";
@@ -24,6 +22,7 @@ export class AuthController {
   @Post("signin")
   async signIn(@Session() session: UserSession, @Body() dto: SignInDto) {
     await this.authService.signIn(session, dto);
+    console.log("Session ", session);
     return;
   }
 
